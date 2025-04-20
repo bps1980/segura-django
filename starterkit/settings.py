8d19ffe8d012d5511a090bef48824d549751b0e9
Brennen Segura <brennen@seguramgmt.com>
2025-04-19 21:22:35 -0500

    Static Files Update

--- starterkit/settings.py
+++ starterkit/settings.py
@@ -75,6 +75,7 @@ MIDDLEWARE = [
     'htmlmin.middleware.MarkRequestMiddleware', 
 
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -175,15 +176,18 @@ HTML_MINIFY = False
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [STATIC_DIR]
-print(STATIC_DIR)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # where collectstatic will put everything
 
-# Media files (user uploads like ID/selfie)
+# Optional: if you have a custom static dir (like 'static') inside your project
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # only if you use a 'static/' folder in your project
+]
+
+# Media files (user uploads)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
